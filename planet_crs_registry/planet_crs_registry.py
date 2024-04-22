a359b2f218b84fb7b28dd2002dc7cb73c1783d21
Emile-Muller <emile.muller.contact@gmail.com>
2024-04-22 09:25:57 +0200

    feat(config): Updated config to not call OpenTelemetry in the Python code, but in the compose.yaml instead

--- planet_crs_registry/planet_crs_registry.py
+++ planet_crs_registry/planet_crs_registry.py
@@ -24,13 +24,6 @@ import ssl
 
 import uvicorn  # type: ignore
 from fastapi import FastAPI
-from opentelemetry import trace
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.resources import SERVICE_NAME
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from uvicorn.config import LOGGING_CONFIG
 
 from ._version import __name_soft__
@@ -62,26 +55,11 @@ class PlanetCrsRegistryLib:
         )
         self.__config.read(self.__path_to_conf)
 
-        ############################
-        # OpenTelemetry and Jaeger #
-        trace.set_tracer_provider(
-            TracerProvider(
-                resource=Resource.create({SERVICE_NAME: "CRS-registry"})
-            )
-        )
-        tracer_provider = trace.get_tracer_provider()
-        jaeger_exporter = JaegerExporter(
-            agent_host_name="jaeger", agent_port=6831
-        )
-        tracer_provider.add_span_processor(BatchSpanProcessor(jaeger_exporter))
-
         self.__app = FastAPI(
             title=openapi_config.name,
             version=openapi_config.version,
             description=openapi_config.description,
         )
-        FastAPIInstrumentor.instrument_app(self.__app)
-        ############################
 
     @staticmethod
     def _parse_level(level: str):
